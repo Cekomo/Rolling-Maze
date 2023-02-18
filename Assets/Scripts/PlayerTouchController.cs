f07e71ee4daf13ad1d5ad57bf042d2f62d70f144
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-02-18 10:41:47 +0300

    fix touchcontroller class

--- Assets/Scripts/PlayerTouchController.cs
+++ Assets/Scripts/PlayerTouchController.cs
@@ -1,38 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+// in this form, finger must be up after swipe operation for ball to move
 
 public class PlayerTouchController : MonoBehaviour
 {
-    private Vector2 fingerDownPosition;
-    private Vector2 fingerUpPosition;
+    private Vector2 _fingerDownPosition;
+    private Vector2 _fingerUpPosition;
     
-    private const int MIN_SWIPE_DISTANCE = 20;
+    // handle the constant below, pixel ratio varies from phone to phone
+    private const int MIN_SWIPE_DISTANCE = 30;
 
-    private SwipeDirection swipeDirection;
+    private SwipeDirection _swipeDirection;
 
     public SwipeDirection SwipeDirection
     {
-        set => swipeDirection = value;
+        set => _swipeDirection = value;
     }
     
     private void Update()
     {
-        foreach (Touch touch in Input.touches)
+        if (Input.touchCount == 0) return;
+        
+        foreach (var touch in Input.touches)
         {
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    fingerUpPosition = touch.position;
-                    fingerDownPosition = touch.position;
-                    swipeDirection = SwipeDirection.None;
-                    break;
-                case TouchPhase.Moved:
-                    fingerDownPosition = touch.position;
-                    DetectSwipe();
+                    _fingerDownPosition = touch.position;
+                    _swipeDirection = SwipeDirection.None;
                     break;
                 case TouchPhase.Ended:
-                    fingerDownPosition = touch.position;
+                    _fingerUpPosition = touch.position;
                     DetectSwipe();
                     break;
             }
@@ -43,44 +41,30 @@ public class PlayerTouchController : MonoBehaviour
     {
         if (!SwipeDistanceCheck()) return;
         
-        swipeDirection = GetSwipeDirection();
-        if (swipeDirection != SwipeDirection.None)
+        _swipeDirection = GetSwipeDirection();
+        if (_swipeDirection != SwipeDirection.None)
         {
-            Debug.Log("Swipe Direction: " + swipeDirection);
+            Debug.Log("Swipe Direction: " + _swipeDirection);
         }
-
-        fingerDownPosition = fingerUpPosition;
     }
 
     private bool SwipeDistanceCheck()
     {
-        return Vector3.Distance(fingerDownPosition, fingerUpPosition) > MIN_SWIPE_DISTANCE;
+        return Vector3.Distance(_fingerDownPosition, _fingerUpPosition) > MIN_SWIPE_DISTANCE;
     }
 
     private SwipeDirection GetSwipeDirection()
     {
-        Vector2 swipeVector = fingerDownPosition - fingerUpPosition;
+        var swipeVector = _fingerDownPosition - _fingerUpPosition;
 
-        // if (swipeVector.magnitude < MIN_SWIPE_DISTANCE)
-        // {
-        //     return SwipeDirection.None;
-        // }
-
-        var angle = Vector2.Angle(swipeVector, Vector2.up);
-
-        switch (Mathf.RoundToInt(angle / 45f))
+        var angle = Vector2.Angle(swipeVector, Vector2.up); // angle is 0 to 180    
+        
+        switch (Mathf.CeilToInt(angle / 45f))
         {
-            case 0:
-            case 8:
+            case 4:
                 return SwipeDirection.Up;
-            case 1:
-            case 2:
-            case 3:
-                return SwipeDirection.Right;
-            case 5:
-            case 6:
-            case 7:
-                return SwipeDirection.Left;
+            case 2: case 3:
+                return swipeVector.x > 0 ? SwipeDirection.Left : SwipeDirection.Right;
             default:
                 return SwipeDirection.None;
         }
