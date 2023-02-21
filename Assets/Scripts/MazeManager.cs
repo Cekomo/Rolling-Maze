f8cc6018bf1daa37dbc8a1f02b113470aa0ea2ce
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-02-21 13:34:10 +0300

    tag children of the maze from script

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -14,9 +14,21 @@ public class MazeManager : MonoBehaviour
         CurrentMaze = Instantiate(mazeModels.mazeModelList[0], mazeContainer);
         CurrentMaze.transform.localScale = new Vector3(2, 2, 2);
         AddColliderToChildren();
+        TagChildren();
     }
 
-    private void AddColliderToChildren()
+    private static void TagChildren()
+    {
+        var mazeTransform = CurrentMaze.transform;
+        for (var i = 0; i < mazeTransform.childCount; i++)
+        {
+            mazeTransform.GetChild(i).tag = "Wall";
+            if (mazeTransform.GetChild(i).name == "Cylinder") 
+                mazeTransform.GetChild(i).tag = "Floor";
+        }
+    }
+
+    private static void AddColliderToChildren()
     {
         MeshFilter[] meshFilters = CurrentMaze.GetComponentsInChildren<MeshFilter>();
         foreach (var filter in meshFilters)
