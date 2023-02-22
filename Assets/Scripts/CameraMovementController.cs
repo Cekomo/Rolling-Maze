5291e4755d08f7eae6de29887dac50dfd9c6c39d
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-02-22 21:52:43 +0300

    add initial camera position for map inspection

--- Assets/Scripts/CameraMovementController.cs
+++ Assets/Scripts/CameraMovementController.cs
@@ -1,12 +1,17 @@
+using System;
 using UnityEngine;
 
 public class CameraMovementController : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    private static readonly Vector3 OFFSET = new(0f, 10f, -10f);
+    
+    // below component must be dynamic wrt maze radius
+    private static readonly Vector3 PausedOffset = new(0, 35f, -25f);
+    private static readonly Vector3 FollowingOffset = new(0f, 10f, -10f);
 
     private void LateUpdate()
     {
-        transform.position = target.position + OFFSET;
+        var currentOffset = LevelLoader.IsPaused ? PausedOffset : FollowingOffset;
+        transform.position = target.position + currentOffset;
     }
 }
