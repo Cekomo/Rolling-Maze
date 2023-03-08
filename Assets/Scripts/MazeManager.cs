231e6b993495c29fb7574b1ff506b6b13af87036
cekomo <cemil.sahin@ozu.edu.tr>
2023-03-08 21:31:29 +0300

    launch MVP version

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -18,36 +18,36 @@
         public void InstantiateNewMaze()
         {
             if (CurrentMaze != null) Destroy(CurrentMaze);
-            CurrentMaze = Instantiate(mazeModels.mazeModelList[LevelLoader.GetLevel()], mazeContainer);
+            CurrentMaze = Instantiate(mazeModels.mazeModelList[LevelLoader.GetLevel()*5], mazeContainer); // * 5 MVP
             
             CurrentMaze.transform.position = new Vector3(0, -4, 0);
             CurrentMaze.transform.localScale = new Vector3(2, 2, 2);
         }
 
-        public static void DecideMazeColor()
-        {
-            var currentLevel = PlayerPrefs.GetInt("LevelIndex") % 20; // % 20 => temporary
-            
-            switch (currentLevel)
-            {
-                case 0:
-                    PaintTheMaze(MazeModels.ColorDict[Colors.DarkGray], MazeModels.ColorDict[Colors.Gray]);
-                    // PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Blue]);
-                    break;
-                case <= 5 and > 0:
-                    PaintTheMaze(MazeModels.ColorDict[Colors.Teal], MazeModels.ColorDict[Colors.Navy]);
-                    break;
-                case <= 10 and > 5:
-                    PaintTheMaze(MazeModels.ColorDict[Colors.Mustard], MazeModels.ColorDict[Colors.DarkGreen]);
-                    break;
-                case <= 15 and > 10:
-                    PaintTheMaze(MazeModels.ColorDict[Colors.Maroon], MazeModels.ColorDict[Colors.Peach]);
-                    break;
-                case <= 20 and > 15:
-                    PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Navy] );
-                    break;
-            }
-        }
+        // public static void DecideMazeColor()
+        // {
+        //     var currentLevel = PlayerPrefs.GetInt("LevelIndex") % 20; // % 20 => temporary
+        //     
+        //     switch (currentLevel)
+        //     {
+        //         case 0:
+        //             PaintTheMaze(MazeModels.ColorDict[Colors.DarkGray], MazeModels.ColorDict[Colors.Gray]);
+        //             // PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Blue]);
+        //             break;
+        //         case <= 5 and > 0:
+        //             PaintTheMaze(MazeModels.ColorDict[Colors.Teal], MazeModels.ColorDict[Colors.Navy]);
+        //             break;
+        //         case <= 10 and > 5:
+        //             PaintTheMaze(MazeModels.ColorDict[Colors.Mustard], MazeModels.ColorDict[Colors.DarkGreen]);
+        //             break;
+        //         case <= 15 and > 10:
+        //             PaintTheMaze(MazeModels.ColorDict[Colors.Maroon], MazeModels.ColorDict[Colors.Peach]);
+        //             break;
+        //         case <= 20 and > 15:
+        //             PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Navy] );
+        //             break;
+        //     }
+        // }
 
         private static void PaintTheMaze(Color floorColor, Color wallColor)
         {
@@ -85,4 +85,31 @@
             
             mazeTransform.GetChild(mazeTransform.childCount - 1).tag = "Floor";
         }
+        
+        // /* MVP edition
+        public static void DecideMazeColor()
+        {
+            var currentLevel = PlayerPrefs.GetInt("LevelIndex") * 5;
+
+            switch (currentLevel)
+            {
+                case 0:
+                    PaintTheMaze(MazeModels.ColorDict[Colors.DarkGray], MazeModels.ColorDict[Colors.Gray]);
+                    // PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Blue]);
+                    break;
+                case <= 5 and > 0:
+                    PaintTheMaze(MazeModels.ColorDict[Colors.Teal], MazeModels.ColorDict[Colors.Navy]);
+                    break;
+                case <= 10 and > 5:
+                    PaintTheMaze(MazeModels.ColorDict[Colors.Mustard], MazeModels.ColorDict[Colors.DarkGreen]);
+                    break;
+                case <= 15 and > 10:
+                    PaintTheMaze(MazeModels.ColorDict[Colors.Maroon], MazeModels.ColorDict[Colors.Peach]);
+                    break;
+                case <= 20 and > 15:
+                    PaintTheMaze(MazeModels.ColorDict[Colors.Orange], MazeModels.ColorDict[Colors.Navy]);
+                    break;
+            }
+        }
+        // */
     }
