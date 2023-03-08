a3bc35a057b67ea0e213cd6f864cf343fab9161a
cekomo <cemil.sahin@ozu.edu.tr>
2023-03-08 15:28:26 +0300

    add decidemazecolor method

--- Assets/Scripts/MazeManager.cs
+++ Assets/Scripts/MazeManager.cs
@@ -22,8 +22,35 @@
             CurrentMaze.transform.position = new Vector3(0, -4, 0);
             CurrentMaze.transform.localScale = new Vector3(2, 2, 2);
         }
-        
-        
+
+        public static void DecideMazeColor()
+        {
+            var currentLevel = PlayerPrefs.GetInt("LevelIndex") % 20; // % 20 => temporary
+            
+            switch (currentLevel)
+            {
+                case 0:
+                    PaintTheMaze(Colors.Orange , Colors.Blue);
+                    break;
+                case <= 5 and > 0:
+                    PaintTheMaze(Colors.Green , Colors.Pink );
+                    break;
+                case <= 10 and > 5:
+                    PaintTheMaze(Colors.Yellow , Colors.Purple );
+                    break;
+                case <= 15 and > 10:
+                    PaintTheMaze(Colors.DarkGray , Colors.LightBlue );
+                    break;
+                case <= 20 and > 15:
+                    PaintTheMaze(Colors.Gray, Colors.DarkGray);
+                    break;
+            }
+        }
+
+        private static void PaintTheMaze(Colors floorColor, Colors wallColor)
+        {
+            
+        }
 
         // not used since mobile does not detect collision in run-time collision addition
         public static void PrepareTheMaze()
--- Assets/Scripts/MazeModels.cs
+++ Assets/Scripts/MazeModels.cs
@@ -9,13 +9,22 @@ public class MazeModels : MonoBehaviour
     public List<GameObject> mazeModelList;
     public static int[] MazeScaleList;
 
-    public List<Color> mazeColorList;
+    public static Dictionary<Colors, int> MazeColorList;
 
     private void Awake()
      { 
         PlayerPrefs.SetInt("LevelIndex", 18);
         MaximumLevel = mazeModelList.Count - 1;
         MazeScaleList = new int[MaximumLevel + 1];
+
+        MazeColorList = new Dictionary<Colors, int> // () ?
+        {
+            { Colors.Red, 0xFF0000 }, { Colors.Green, 0x00FF00 }, { Colors.Blue, 0x0000FF }, 
+            { Colors.Yellow, 0xFFFF00 }, { Colors.Orange, 0xFFA500 }, { Colors.Magenta, 0xFF00FF }, 
+            { Colors.Pink, 0xFFC0CB }, { Colors.Purple, 0x800080 }, { Colors.Cyan, 0x00FFFF }, 
+            { Colors.Gray, 0x808080 }, { Colors.LightBlue, 0xADD8E6 }, { Colors.LightYellow, 0xFFFFE0 }, 
+            { Colors.DarkGray, 0xA9A9A9 }, { Colors.DarkGreen, 0x006400 } 
+        };
 
         // levelCount+2 is seems good for initial camera distance
         MazeScaleList = new[] { 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9};
