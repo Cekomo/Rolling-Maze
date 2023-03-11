7fe7c17cf7c10633d4adb11af71bad1ae60e2b45
cekomo <cemil.sahin@ozu.edu.tr>
2023-03-11 15:48:24 +0300

    add toggleMute button & functionality

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,25 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject startPanelGuide;
     [SerializeField] private GameObject startPanelDefault;
     [SerializeField] private TMP_Text levelCount;
+    
+    private static bool IsMuted;
+    [SerializeField] private Button audioButton;
+    private Image unmuteImage;
+    private Image muteImage;
 
     private void Start()
     {
         SetLevelCounter();
         SetStartPanelStatus(true);
+        
+        unmuteImage = audioButton.transform.GetChild(0).GetComponent<Image>();
+        muteImage = audioButton.transform.GetChild(1).GetComponent<Image>();
     }
 
     private void Update() // transfer this if you create a class like gameManager 
@@ -32,5 +41,14 @@ public class UIManager : MonoBehaviour
     public void SetLevelCounter()
     {
         levelCount.text = "Level " + (LevelLoader.GetLevel() + 1);
+    }
+
+    public void ToggleMute()
+    {
+        IsMuted = !IsMuted;
+        AudioListener.pause = IsMuted;
+
+        unmuteImage.gameObject.SetActive(!IsMuted);
+        muteImage.gameObject.SetActive(IsMuted);
     }
 }
