1944e5b35702949e22d1c22f0fef2843c06b6f17
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-03-14 23:55:33 +0300

    inter-commit to unlock skins

--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -7,25 +7,57 @@ public class SkinManager : MonoBehaviour
     public static int LevelPoint;
 
     [SerializeField] private GameObject ball;
-    private int currentBallIndex;
+    private int _currentBallIndex;
     [HideInInspector] public Material currentBallSkin;
     [SerializeField] private Material[] ballSkins;
+    private int[] _ballSkinCosts;
+    // private bool[] _purchasedBallSkins;
 
     private void Awake()
     {
+        // PlayerPrefs.SetInt("CurrentSkinIndex", 0);
+        // PlayerPrefs.SetInt("GamePoint", 250);
+        _ballSkinCosts = new int[5];
+        _ballSkinCosts = new[] { 0, 200, 300, 400, 500 };
+        
+        // var unlockedSkins = "1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+        print(PlayerPrefs.GetString("UnlockedSkins"));
         SetBallSkin();
     }
 
     private void SetBallSkin()
     {
-        currentBallIndex = PlayerPrefs.GetInt("CurrentSkinIndex");
-        currentBallSkin = ballSkins[currentBallIndex];
+        _currentBallIndex = PlayerPrefs.GetInt("CurrentSkinIndex");
+        currentBallSkin = ballSkins[_currentBallIndex];
         ball.GetComponent<MeshRenderer>().material = currentBallSkin;
     }
 
     public void ChangeBallSkin(int skinIndex)
     {
-        PlayerPrefs.SetInt("CurrentSkinIndex", skinIndex);
-        SetBallSkin();
+        var unlockedSkinsArray = PlayerPrefs.GetString("UnlockedSkins");
+        
+        if (unlockedSkinsArray[skinIndex * 2] == 1)
+            SetBallSkin();
+        else 
+            UnlockBallSkin(skinIndex, unlockedSkinsArray);
+    }
+
+    private void UnlockBallSkin(int skinIndex, string unlockedSkins)
+    {
+        if (GamePoint < _ballSkinCosts[skinIndex]) return;
+        GamePoint -= _ballSkinCosts[skinIndex];
+        var newUnlockedSkins = "";
+        for (var i = 0; i < unlockedSkins.Length; i++)
+        {
+            if (i == skinIndex * 2)
+            {
+                newUnlockedSkins += "1,";
+                i++;
+                continue;
+            }
+            newUnlockedSkins += unlockedSkins[i];
+        }
+        print("zort");
+        PlayerPrefs.SetString("UnlockedSkins", newUnlockedSkins);
     }
 }
